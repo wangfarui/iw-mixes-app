50e1638e9be602f69284f6c40d383b257be60788
bubu <farui.wang@outlook.com>
2025-04-03 16:06:55 +0800

    adjust:调整tabBar

---
+++ pages/bill/index.vue
@@ -0,0 +1,17 @@
+<!-- 账单 -->
+<template>
+	<view>
+		<view class="container">
+			<text>账单</text>
+		</view>
+	</view>
+</template>
+
+<script setup>
+</script>
+
+<style>
+	.container {
+		padding: 10px;
+	}
+</style> 
